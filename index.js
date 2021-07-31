19bcfda8f57d293e59899c6a8c9a759861bfd2a8
Wyatt Hoffman <wyatthoffman4392@gmail.com>
2021-07-31 07:15:33 -0700

    feat: body-parser and get request added

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+// imports
 const express = require("express");
+const bodyParser = require('body-parser');
 
+// create express app
 const app = express();
 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// get request routing
+app.get('/', (req, res) => {
+    res.json({'message': 'test success message body received'})
+});
+
+// request listener
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
